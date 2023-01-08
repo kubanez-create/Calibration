ab419b4ff75f8e62a3824e37ce7b3e978d67c361
Stanislav <zo22@mail.ru>
2023-01-08 16:14:59 +0300

    database works

--- homework.py
+++ homework.py
@@ -4,198 +4,235 @@ from __future__ import annotations
 import logging
 import os
 import sys
-import time
 from logging import StreamHandler
-from typing import Any, Union
-from http import HTTPStatus
 
-import requests
+from telethon import TelegramClient, events, Button # pip install telethon
+from datetime import datetime
 import mysql.connector
-import telegram
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from dotenv import load_dotenv
-
-
-from myexceptions import (
-    APIExceptionError,
-    BadRequestError,
-    MissingKyeError,
-    TokenExceptionError,
-)
 
 load_dotenv()
 
-
 TELEGRAM_TOKEN: str = str(os.getenv("TELEGRAM_TOKEN"))
-TELEGRAM_CHAT_ID: Union[str, None] = os.getenv("TELEGRAM_CHAT_ID")
+API_HASH: str = str(os.getenv("API_HASH"))
+API_ID: int = int(os.getenv("API_ID"))
+USER: str = str(os.getenv("USER"))
+PASSWORD: str = str(os.getenv("PASSWORD"))
+HOST: str = str(os.getenv("HOST"))
+PORT: str = str(os.getenv("PORT"))
+DATABASE: str = str(os.getenv("DATABASE"))
 
-RETRY_PERIOD: int = 600
-WAITING_TIME: int = 500
+SESSION_NAME = "sessions/Bot"
 
+# Start the Client (telethon)
+client = TelegramClient(SESSION_NAME, API_ID, API_HASH).start(bot_token=TELEGRAM_TOKEN)
 
 def check_tokens() -> bool:
     """Function which ckecks availability of global variables."""
-    return all((TELEGRAM_TOKEN, TELEGRAM_CHAT_ID))
+    return all((TELEGRAM_TOKEN, API_HASH, API_ID))
 
-def send_form(update, context):
-    # Create the form
-    form = []
-    form.append(['Name:', 'Age:'])
-    form.append(['', ''])
-
-    # Send the form
-    update.message.reply_text('Please fill out the following form:', reply_markup=form)
-
-def send_message(bot: telegram.Bot, message: str) -> None:
-    """Function which sends a message to a telegram chat.
-
-    Telegram chat defined in TELEGRAM_CHAT_ID global variable.
-
-    Args:
-        bt (telegram.Bot): an instance of a Telegram bot class
-        message (str): text of a message which is to be sent to the chat
-    """
-    try:
-        bot.send_message(
-            chat_id=TELEGRAM_CHAT_ID,
-            text=f"{message}",
-        )
-        logging.debug("We have started sending a message")
-    except Exception as error:
-        logging.error(
-            f"An error occured during sending of a message. Error: {error}"
-        )
-    logging.debug("Message successfully sent to the user")
-
-
-def get_api_answer(timestamp: int) -> dict[str, Any]:
-    """Function which sends request to the API's endpoint.
-
-    It returns the API's response cast to a python's dict.
-
-    Args:
-        timestamp (int): starting Unix time to check homework
-
-    Returns:
-        dict[str, Any]: API's response casted to python dict
-    """
-    url: str = "https://practicum.yandex.ru/api/user_api/homework_statuses/"
-    headers: dict[str, str] = {"Authorization": f"OAuth {PRACTICUM_TOKEN}"}
-    payload: dict[str, int] = {"from_date": timestamp}
-
-    try:
-        logging.debug("We have sended a request to an API")
-        homework_statuses: requests.Response = requests.get(
-            url, headers=headers, params=payload, timeout=WAITING_TIME
-        )
-    except requests.RequestException:
-        raise BadRequestError("RequestException error")
-    status: int = homework_statuses.status_code
-    if status != HTTPStatus.OK:
-        raise APIExceptionError(
-            f"Server returned error with the status {status}"
-        )
-    return homework_statuses.json()
-
-
-def check_response(response: dict[str, Any]) -> list[dict[str, str]]:
-    """Checks if API's response complies with documentation.
-
-    Args:
-        response (dict): API's response casted to a dict type
-
-    Returns:
-        list[dict[str, str]]: python list of dictionaries
-    """
-    if not isinstance(response, dict):
-        raise TypeError("Response must be a python dictionary but it is not")
-    if "current_date" not in response:
-        raise MissingKyeError("Response lacks the 'current_date' key")
-    if "homeworks" not in response:
-        raise MissingKyeError("Response lacks the 'homeworks' key")
-
-    homeworks = response.get("homeworks")
-
-    if not isinstance(homeworks, list):
-        raise TypeError("Response['homeworks'] is not a python list")
-
-    return homeworks
-
-
-def parse_status(homework: dict[str, str]) -> str:
-    """Function which processes json response into python format.
-
-    It takes information abour some specific homework and returns
-    python string which contains this information.
-
-    Args:
-        homework (dict[str, str]): API's response
-        related to some particular homework casted to a python dict
-
-    Returns:
-        str: python string
-    """
-    if "homework_name" not in homework:
-        raise MissingKyeError(
-            "An API's response lacks the 'homework_name' key"
-        )
-    if "status" not in homework:
-        raise MissingKyeError(
-            "An API's response lacks the 'status' key"
-        )
-    if homework.get("status") not in HOMEWORK_VERDICTS:
-        raise MissingKyeError(
-            "An API's responded with an unknown homework status"
-        )
-    homework_name: str = homework["homework_name"]
-    homework_status: str = homework["status"]
-    verdict: str = HOMEWORK_VERDICTS[homework_status]
-
-    return f'Изменился статус проверки работы "{homework_name}". {verdict}'
-
-
-def main() -> None:
-    logging.basicConfig(
-        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-    )
-    logger: logging.Logger = logging.getLogger(__name__)
-    logger.setLevel(logging.DEBUG)
-    handler: StreamHandler = StreamHandler(stream=sys.stdout)
-    logger.addHandler(handler)
-
-    # Connect to the database
-    cnx = mysql.connector.connect(
-        host='YOUR_DB_HOST',
-        user='YOUR_DB_USER',
-        password='YOUR_DB_PASSWORD',
-        database='YOUR_DB_NAME'
-    )
-
-    # Create a cursor
-    cursor = cnx.cursor()
-
-    # Create the Updater and pass it your bot's token.
-    updater = Updater(TELEGRAM_TOKEN, use_context=True)
-
-    # Get the dispatcher to register handlers
-    dp = updater.dispatcher
-
-    # Add a command handler to send the form
-    dp.add_handler(CommandHandler('send_form', send_form))
-
-    # Add a message handler to handle form submissions
-    dp.add_handler(MessageHandler(Filters.text, form_submission))
-
-    # Start the Bot
-    updater.start_polling()
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
-    updater.idle()
-
-    """Основная логика работы бота."""
+### START COMMAND
+@client.on(events.NewMessage(pattern="(?i)/start"))
+async def start(event):
+    # Get sender
+    sender = await event.get_sender()
+    SENDER = sender.id
     
+    # set text and send message
+    markup = event.client.build_reply_markup([
+        [Button.text('First button', resize=True, single_use=True)],
+    ])
+    text = "Hello i am a bot that can do CRUD operations inside a MySQL database"
+    await client.send_message(SENDER, text, buttons=markup)
+
+
+### Insert command
+@client.on(events.NewMessage(pattern="(?i)/insert"))
+async def insert(event):
+    try:
+        # Get the sender of the message
+        sender = await event.get_sender()
+        SENDER = sender.id
+
+        # /insert bottle 10
+
+        # Get the text of the user AFTER the /insert command and convert it to a list (we are splitting by the SPACE " " simbol)
+        list_of_words = event.message.text.split("; ")
+        date = datetime.now().strftime("%d/%m/%Y") # Use the datetime library to the get the date (and format it as DAY/MONTH/YEAR)
+        task_description = list_of_words[1]
+        prediction_50_percent_sure = list_of_words[2]
+        prediction_90_percent_sure = list_of_words[3]
+        actual = None
+
+        # Create the tuple "params" with all the parameters inserted by the user
+        params = (date, task_description, prediction_50_percent_sure,
+                  prediction_90_percent_sure, actual)
+        sql_command = """INSERT INTO raw_predictions VALUES
+                         (NULL, %s, %s, %s, %s, %s);""" # the initial NULL is for the AUTOINCREMENT id inside the table
+        crsr.execute(sql_command, params) # Execute the query
+        conn.commit() # commit the changes
+
+        # If at least 1 row is affected by the query we send specific messages
+        if crsr.rowcount < 1:
+            text = "Something went wrong, please try again"
+            await client.send_message(SENDER, text, parse_mode='html')
+        else:
+            text = "Order correctly inserted"
+            await client.send_message(SENDER, text, parse_mode='html')
+
+    except Exception as e: 
+        print(e)
+        await client.send_message(SENDER, "Something Wrong happened... Check your code!", parse_mode='html')
+        return
+
+
+
+# Function that creates a message containing a list of all the oders
+def create_message_select_query(ans):
+    text = ""
+    for i in ans:
+        id = i[0]
+        product = i[1]
+        quantity = i[2]
+        creation_date = i[3]
+        text += "<b>"+ str(id) +"</b> | " + "<b>"+ str(product) +"</b> | " + "<b>"+ str(quantity)+"</b> | " + "<b>"+ str(creation_date)+"</b>\n"
+    message = "<b>Received 📖 </b> Information about orders:\n\n"+text
+    return message
+
+### SELECT COMMAND
+@client.on(events.NewMessage(pattern="(?i)/select"))
+async def select(event):
+    try:
+        # Get the sender of the message
+        sender = await event.get_sender()
+        SENDER = sender.id
+        # Execute the query and get all (*) the oders
+        crsr.execute("SELECT * FROM orders")
+        res = crsr.fetchall() # fetch all the results
+        # If there is at least 1 row selected, print a message with the list of all the oders
+        # The message is created using the function defined above
+        if(res):
+            text = create_message_select_query(res) 
+            await client.send_message(SENDER, text, parse_mode='html')
+        # Otherwhise, print a default text
+        else:
+            text = "No orders found inside the database."
+            await client.send_message(SENDER, text, parse_mode='html')
+
+    except Exception as e: 
+        print(e)
+        await client.send_message(SENDER, "Something Wrong happened... Check your code!", parse_mode='html')
+        return
+
+
+
+### UPDATE COMMAND
+@client.on(events.NewMessage(pattern="(?i)/update"))
+async def update(event):
+    try:
+        # Get the sender
+        sender = await event.get_sender()
+        SENDER = sender.id
+
+        # Get the text of the user AFTER the /update command and convert it to a list (we are splitting by the SPACE " " simbol)
+        list_of_words = event.message.text.split(" ")
+        id = int(list_of_words[1]) # second (1) item is the id
+        new_product = list_of_words[2] # third (2) item is the product
+        new_quantity = list_of_words[3] # fourth (3) item is the quantity
+        dt_string = datetime.now().strftime("%d/%m/%Y") # We create the new date
+
+        # create the tuple with all the params interted by the user
+        params = (id, new_product, new_quantity, dt_string, id)
+
+        # Create the UPDATE query, we are updating the product with a specific id so we must put the WHERE clause
+        sql_command="UPDATE orders SET id=%s, product=%s, quantity=%s, LAST_EDIT=%s WHERE id =%s"
+        crsr.execute(sql_command, params) # Execute the query
+        conn.commit() # Commit the changes
+
+        # If at least 1 row is affected by the query we send a specific message
+        if crsr.rowcount < 1:
+            text = "Order with id {} is not present".format(id)
+            await client.send_message(SENDER, text, parse_mode='html')
+        else:
+            text = "Order with id {} correctly updated".format(id)
+            await client.send_message(SENDER, text, parse_mode='html')
+
+    except Exception as e: 
+        print(e)
+        await client.send_message(SENDER, "Something Wrong happened... Check your code!", parse_mode='html')
+        return
+
+
+
+@client.on(events.NewMessage(pattern="(?i)/delete"))
+async def delete(event):
+    try:
+        # Get the sender
+        sender = await event.get_sender()
+        SENDER = sender.id
+
+        #/ delete 1
+
+        # get list of words inserted by the user
+        list_of_words = event.message.text.split(" ")
+        id = list_of_words[1] # The second (1) element is the id
+
+        # Crete the DELETE query passing the id as a parameter
+        sql_command = "DELETE FROM orders WHERE id = (%s);"
+
+        # ans here will be the number of rows affected by the delete
+        ans = crsr.execute(sql_command, (id,))
+        conn.commit()
+        
+        # If at least 1 row is affected by the query we send a specific message
+        if ans < 1:
+            text = "Order with id {} is not present".format(id)
+            await client.send_message(SENDER, text, parse_mode='html')
+        else:
+            text = "Order with id {} was correctly deleted".format(id)
+            await client.send_message(SENDER, text, parse_mode='html')
+
+    except Exception as e: 
+        print(e)
+        await client.send_message(SENDER, "Something Wrong happened... Check your code!", parse_mode='html')
+        return
+
 
 if __name__ == "__main__":
-    main()
+    try:
+        logging.basicConfig(
+            format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        )
+        logger: logging.Logger = logging.getLogger(__name__)
+        logger.setLevel(logging.DEBUG)
+        handler: StreamHandler = StreamHandler(stream=sys.stdout)
+        logger.addHandler(handler)
+
+        # Connect to the database
+        conn = mysql.connector.connect(
+            host=HOST,
+            user=USER,
+            password=PASSWORD,
+            database=DATABASE
+        )
+
+        # Create a cursor
+        crsr = conn.cursor()
+
+        # Command that creates the "oders" table 
+        sql_command = """CREATE TABLE IF NOT EXISTS raw_predictions ( 
+            id INTEGER PRIMARY KEY AUTO_INCREMENT, 
+            date VARCHAR(100),
+            task_description VARCHAR(200),
+            prediction_50_percent_sure INT(10),
+            prediction_90_percent_sure INT(10), 
+            actual_outcome INT(10));"""
+
+        crsr.execute(sql_command)
+        print("All tables are ready")
+        
+        print("Bot Started...")
+        client.run_until_disconnected()
+
+    except Exception as error:
+        print('Cause: {}'.format(error))
