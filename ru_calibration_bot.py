d42612031b99d423a502fda5bbd76aba735ad723
Stanislav <zo22@mail.ru>
2023-02-10 14:12:06 +0300

    updated delete method

--- ru_calibration_bot.py
+++ ru_calibration_bot.py
@@ -156,6 +156,47 @@ async def guide(event):
         return
 
 
+@client.on(events.NewMessage(pattern="Мои"))
+async def display_categories(event):
+    """Show to a user their unique categories.
+
+    Which they have already inputed.
+
+    Args:
+        event (EventCommon): NewMessage event
+    """
+    try:
+        sender = await event.get_sender()
+        SENDER = sender.id
+        query = (
+            "SELECT DISTINCT task_category FROM"
+            " predictions.raw_predictions WHERE user_id = %s"
+        )
+        crsr.execute(query, [SENDER])
+        res = crsr.fetchall()  # fetch all the results
+        # If there is at least 1 row selected, print a message with the list
+        # of all predictions
+        if res:
+            text = create_message_categories(res)
+            await client.send_message(SENDER, text, parse_mode="html")
+        # Otherwhise, print a default text
+        else:
+            text = (
+                "Складывается впечатление, что вы еще не внесли ни одного"
+                " предсказания. Попробуйте."
+            )
+            await client.send_message(SENDER, text, parse_mode="html")
+            logger.debug("Someone tried to have a look at their categories"
+                         " without any made predictions.")
+
+    except Exception as e:
+        logger.error(
+            "Something went wrong when showing user's predictions "
+            f"with an error: {e}"
+        )
+        return
+
+# Detail view block: update, delete, list, enter outcome methods
 @client.on(events.NewMessage(pattern=validate_checking))
 async def CUEDhandler(event):
     sender = await event.get_sender()
@@ -176,7 +217,6 @@ async def CUEDhandler(event):
         await event.respond(text)
 
     elif state == State.WAIT_CHECK:
-        # get a list of user's categories!
         query = (
             "SELECT DISTINCT task_category FROM"
             " predictions.raw_predictions WHERE user_id = %s"
@@ -199,7 +239,9 @@ async def CUEDhandler(event):
                           " категории.")
                 )
                 del conversation_state[who]
-                logger.info("Categories request isn't valid")
+                logger.info(
+                    "Someone tried to get a list of categories"
+                    " but have not made any themselves.")
 
             else:
                 # Execute the query and get all (*) predictions
@@ -326,6 +368,9 @@ async def CUEDhandler(event):
                      " Вами предсказаний. Пожалуйста внесите корректный номер.")
                 )
                 del conversation_state[who]
+                logger.info(
+                    "Someone tried to update a someone else's prediction "
+                )
             else:
                 params = (low_50, hi_50, low_90, hi_90, index)
                 sql_command = """UPDATE predictions.raw_predictions SET
@@ -390,9 +435,14 @@ async def CUEDhandler(event):
                 await client.send_message(
                     who,
                     ("Номер предсказания не совпадает ни с одним из сделанных"
-                     " Вами предсказаний. Пожалуйста внесите корректный номер.")
+                     " Вами предсказаний. Пожалуйста внесите корректный номер."
+                     )
                 )
                 del conversation_state[who]
+                logger.info(
+                    "Someone tried to enter outcome for a someone else's"
+                    " prediction."
+                )
             # Create the tuple "params" with all the parameters inserted
             # by the user
             else:
@@ -410,6 +460,76 @@ async def CUEDhandler(event):
                 logger.info(
                     f"Outcome of the prediction with id {pred_id}"
                     "successfully entered")
+                del conversation_state[who]
+    # DELETE METHOD
+    elif re.match(r"Удалить", mes):
+        conversation_state[who] = State.WAIT_DELETE
+        text = (
+            "Для того, чтобы удалить ранее сделанное предсказание,"
+            " отправьте его номер в ответном сообщении."
+            )
+        await event.respond(text)
+    elif state == State.WAIT_DELETE:
+        query = (
+            "SELECT id FROM"
+            " predictions.raw_predictions WHERE user_id = %s"
+        )
+        crsr.execute(query, [who])
+        user_predictions = crsr.fetchall()  # fetch all the results
+        # If there is no categories yet, print a warning
+        if not user_predictions:
+            text = (
+                "Кажется, Вы еще не сохраняли предсказаний."
+                " Попробуйте."
+            )
+            del conversation_state[who]
+            await client.send_message(who, text, parse_mode="html")
+            logger.info("Someone tried to delete a prediction"
+                        " without making at least one themselves")
+
+        if not validate_outcome(mes):
+            await client.send_message(
+                (
+                    "К сожалению, Ваше сообщение не похоже на цифру."
+                    " Что именно Вы пытаетесь отправить? Попробуйте отправить"
+                    " номер предсказания, которое вы пытаетесь удалить еще раз,"
+                    " пожалуйста."
+                )
+            )
+            logger.info("Outcome message isn't valid")
+            del conversation_state[who]
+
+        else:
+            mes_list = [str(x) for x in chain.from_iterable(user_predictions)]
+            pred_id = mes_list[0]
+            if pred_id not in mes_list:
+                await client.send_message(
+                    who,
+                    ("Номер предсказания не совпадает ни с одним из сделанных"
+                     " Вами предсказаний. Пожалуйста внесите корректный номер.")
+                )
+                del conversation_state[who]
+                logger.info(
+                    "Someone tried to deleto a someone else's prediction."
+                )
+            else:
+                # Create the DELETE query passing the id as a parameter
+                sql_command = """DELETE FROM predictions.raw_predictions
+                WHERE id = (%s);"""
+
+                # ans here will be the number of rows affected by the delete
+                crsr.execute(sql_command, [pred_id])
+                conn.commit()
+                global COUNTER
+                COUNTER -= 1
+                await client.send_message(
+                    who,
+                    (f"Предсказание с номером {pred_id}"
+                     " успешно удалено.")
+                )
+                logger.info(
+                    f"Prediction with id {pred_id}"
+                    "successfully deleted")
                 del conversation_state[who]
 
 
@@ -658,319 +778,6 @@ async def show(event):
         logger.error(
             f"Something went wrong when showing page {page} user's predictions"
             f" with an error: {e}"
-        )
-        return
-
-
-@client.on(events.NewMessage(pattern="My"))
-async def display_categories(event):
-    """Show to a user their unique categories.
-
-    Which they have already inputed.
-
-    Args:
-        event (EventCommon): NewMessage event
-    """
-    try:
-        sender = await event.get_sender()
-        SENDER = sender.id
-        query = (
-            "SELECT DISTINCT task_category FROM"
-            " predictions.raw_predictions WHERE user_id = %s"
-        )
-        crsr.execute(query, [SENDER])
-        res = crsr.fetchall()  # fetch all the results
-        # If there is at least 1 row selected, print a message with the list
-        # of all predictions
-        if res:
-            text = create_message_categories(res)
-            await client.send_message(SENDER, text, parse_mode="html")
-        # Otherwhise, print a default text
-        else:
-            text = (
-                "You have made no predictions so far. Give it a try! "
-                "It is for free."
-            )
-            await client.send_message(SENDER, text, parse_mode="html")
-            logger.debug("Someone tried to have a look at their categories"
-                         " without any made predictions.")
-
-    except Exception as e:
-        logger.error(
-            "Something went wrong when showing user's predictions "
-            f"with an error: {e}"
-        )
-        return
-
-
-
-@client.on(events.NewMessage(pattern="Enter"))
-async def enter(event):
-    """Enter an actual outcome for a made prediction.
-
-    Args:
-        event (EventCommon): NewMessage event
-    """
-    try:
-        sender = await event.get_sender()
-        SENDER = sender.id
-
-        async with client.conversation(
-            await event.get_chat(), exclusive=True
-        ) as conv:
-            text = (
-                "Has reality surprised you this time, eh?"
-                " Anyway just enter the id of your prediction; and"
-                " an actual outcome of an event."
-                " For example: 1; 7"
-            )
-            await conv.send_message(text)
-            response = await conv.get_response(timeout=600)
-            if not validate_outcome(response.text):
-                await conv.send_message(
-                    (
-                        "Sorry but your input isn't valid."
-                        " Please check that your message consists of"
-                        " 2 numbers separated by semicolon and whitespace"
-                        " and also make sure your first number is decimal."
-                    )
-                )
-                logger.info("Outcome message isn't valid")
-            else:
-                list_of_words = response.text.split("; ")
-                user_id = SENDER
-                # Use the datetime library to the get the date
-                # (and format it as DAY/MONTH/YEAR)
-                date = datetime.now().strftime("%d/%m/%Y")
-                pred_id = list_of_words[0]
-                actual_outcome = list_of_words[1]
-
-                # Create the tuple "params" with all the parameters inserted
-                # by the user
-                params = (pred_id, user_id, date, actual_outcome, pred_id)
-                sql_command = """UPDATE predictions.raw_predictions SET id=%s,
-                user_id=%s, date=%s, actual_outcome=%s WHERE id=%s;"""
-
-                crsr.execute(sql_command, params)  # Execute the query
-                conn.commit()  # Commit the changes
-
-                # If at least 1 row is affected by the query we send
-                # a specific message
-                if crsr.rowcount < 1:
-                    text = f"Prediction with id {pred_id} is not present"
-                    await client.send_message(SENDER, text, parse_mode="html")
-                    logger.info("Enter outcome function was aborted"
-                                f" with the text: {text}")
-                else:
-                    text = (
-                        "An actual outcome of an event with "
-                        f"id {pred_id} correctly saved"
-                    )
-                    await client.send_message(SENDER, text, parse_mode="html")
-                    logger.debug(text)
-        await conv.cancel_all()
-        return
-
-    except Exception as e:
-        logger.error(
-            "Something went wrong when entering an outcome"
-            f" with an error: {e}"
-        )
-        return
-
-
-@client.on(events.NewMessage(pattern="Check"))
-async def check(event):
-    """Get user's calibration.
-
-    User can get their overall calibration or choose one
-    particular category.
-
-    Args:
-        event (EventCommon): NewMessage event
-    """
-    try:
-        # Get the sender
-        sender = await event.get_sender()
-        SENDER = sender.id
-
-        async with client.conversation(
-            await event.get_chat(), exclusive=True
-        ) as conv:
-            text = (
-                "Type all and then enter if you'd like to get your current"
-                " overall calibration. In case you're after some specific"
-                " area of your life's calibration just type the category"
-                " and press enter. So either enter 'all' or e.g. 'work'"
-                " as one word without any punctuation."
-            )
-            await conv.send_message(text)
-            response = await conv.get_response(timeout=600)
-            ans: str = response.text
-            if not validate_calibration(response.text):
-                await conv.send_message(
-                    (
-                        "Sorry but your input isn't valid."
-                        " Please make sure it consists of a single word"
-                        " without any numbers or punctuation marks."
-                    )
-                )
-                logger.info("Categories request isn't valid")
-            else:
-
-                # Execute the query and get all (*) predictions
-                if ans.lower() == "all":
-                    query = """SELECT tot_acc_50 / tot_num_pred AS calibration_50,
-                        tot_acc_90 / tot_num_pred AS calibration_90
-                    FROM (
-                        SELECT COUNT(id) AS tot_num_pred,
-                        SUM(acc_50) AS tot_acc_50,
-                        SUM(acc_90) AS tot_acc_90
-                    FROM (
-                        SELECT
-                        id, user_id,
-                        pred_low_50_conf <= actual_outcome AND
-                        pred_high_50_conf >= actual_outcome AS acc_50,
-                        pred_low_90_conf <= actual_outcome AND
-                        pred_high_90_conf >= actual_outcome AS acc_90
-                        FROM predictions.raw_predictions
-                        WHERE user_id = %s AND
-                        actual_outcome IS NOT NULL)
-                        AS base_table
-                    GROUP BY user_id
-                    ) AS outer_table;"""
-                    crsr.execute(query, [SENDER])
-                    res = crsr.fetchall()  # fetch all the results
-
-                    if not res:
-                        await client.send_message(
-                            SENDER,
-                            ("There's currently nothing your calibration"
-                             " might possibly be calculated on. Make at least"
-                             " one prediction and then enter an actual outcome"
-                             " for it."),
-                            parse_mode="html")
-                        return
-
-                    text = (
-                        "Your overall calibration so far:\n"
-                        f"for a 50 percent confidence level - {res[0][0]:.2f}"
-                        "\n"
-                        f"for a 90 percent confidence level - {res[0][1]:.2f}"
-                    )
-                else:
-                    # Or get results for a specific category
-                    query = (
-                        "SELECT tot_acc_50 / tot_num_pred AS calibration_50,"
-                        " tot_acc_90 / tot_num_pred AS calibration_90"
-                        " FROM ("
-                        "SELECT COUNT(id) AS tot_num_pred,"
-                        " SUM(acc_50) AS tot_acc_50,"
-                        " SUM(acc_90) AS tot_acc_90"
-                        " FROM ("
-                        "SELECT"
-                        " id, user_id,"
-                        " pred_low_50_conf <= actual_outcome AND"
-                        " pred_high_50_conf >= actual_outcome AS acc_50,"
-                        " pred_low_90_conf <= actual_outcome AND"
-                        " pred_high_90_conf >= actual_outcome AS acc_90"
-                        " FROM predictions.raw_predictions"
-                        " WHERE user_id = %s AND"
-                        " task_category = %s AND"
-                        " actual_outcome IS NOT NULL)"
-                        " AS base_table"
-                        " GROUP BY user_id"
-                        ") AS outer_table;"
-                    )
-                    crsr.execute(query, (SENDER, ans))
-                    res = crsr.fetchall()  # fetch all the results
-
-                    if not res:
-                        await client.send_message(
-                            SENDER,
-                            ("There's currently nothing your calibration"
-                             " might possibly be calculated on. Make at least"
-                             " one prediction and then enter an actual outcome"
-                             " for it."),
-                            parse_mode="html")
-                        return
-
-                    text = (
-                        "Your overall calibration so far:\n"
-                        " for a 50 percent confidence level"
-                        f" - {res[0][0]:.2f}\n"
-                        f" for a 90 percent confidence level - {res[0][1]:.2f}"
-                    )
-                await client.send_message(SENDER, text, parse_mode="html")
-                logger.debug(f" Check function returned following: {text}")
-        await conv.cancel_all()
-        return
-
-    except Exception as e:
-        logger.error(
-            "Something went wrong in calibration's calculation"
-            f" with an error: {e}"
-        )
-        return
-
-
-@client.on(events.NewMessage(pattern="Delete"))
-async def delete(event):
-    """Delete one of made predictions specified by an id.
-
-    Args:
-        event (EventCommon): NewMessage event
-    """
-    try:
-        # Get the sender
-        sender = await event.get_sender()
-        SENDER = sender.id
-
-        async with client.conversation(
-            await event.get_chat(), exclusive=True
-        ) as conv:
-            text = "Enter an id for a prediction you are going to delete"
-            await conv.send_message(text)
-            response = await conv.get_response(timeout=600)
-            idn = response.text
-            if not validate_deletion(response.text):
-                await conv.send_message(
-                    (
-                        "Sorry but your input isn't valid."
-                        " Please make sure it consists of a single"
-                        " integer and nothing else."
-                    )
-                )
-                logger.info("Deletion request isn't valid")
-            else:
-
-                # Create the DELETE query passing the id as a parameter
-                sql_command = """DELETE FROM predictions.raw_predictions
-                WHERE id = (%s);"""
-
-                # ans here will be the number of rows affected by the delete
-                crsr.execute(sql_command, [idn])
-                conn.commit()
-
-                # If at least 1 row is affected by the query we send
-                # a specific message
-                if crsr.rowcount < 1:
-                    text = f"Prediction with id {idn} is not present"
-                    await client.send_message(SENDER, text, parse_mode="html")
-                    logger.info(f"Deletion was aborted with the text {text}")
-                else:
-                    text = f"Prediction with id {idn} correctly deleted"
-                    await client.send_message(SENDER, text, parse_mode="html")
-                    logger.debug(text)
-                global COUNTER
-                COUNTER -= 1
-        await conv.cancel_all()
-        return
-
-    except Exception as e:
-        logger.error(
-            "Something went wrong when deleting user's prediction "
-            f"with an error: {e}"
         )
         return
 
