8d44c81f72bd6f145db635264a852fa1fcef05af
Stanislav <zo22@mail.ru>
2023-02-17 12:50:54 +0300

    cleaned everything

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# homework_bot
-python telegram bot
+# Calibration bot
+
+People, among other things are famous for their inability to distinguish between things they control, know, can possibly predict and understand and things they don't.
+
+So this bot was developed in hope it will help to better understand one's limits at least when we talk about such cognitive tasks as predicting the future.
+We often make implicit assumptions about our future without properly understanding just how good our past predictions were and so to what extend we can rely on the current intution telling us for an instance that those inky clouds doesn't actually matter so much when the forecast was "clear sky and not a slightest chance of rain tomorrow".
--- helpers.py
+++ helpers.py
@@ -1,7 +1,15 @@
 """Helper functions for the bot."""
 from __future__ import annotations
+from typing import Union
+
+import re
 
 from itertools import chain
+
+SMILES_NUMBER: int = 60
+SMALL_DIAMOND = "\U0001F538"
+PENCIL = "\U0000270F"
+BLACK_SQUARE = "\U000025AA"
 
 
 def create_message_categories(ans: list[str]):
@@ -13,7 +21,9 @@ def create_message_categories(ans: list[str]):
     Returns:
         str: message ready to be sent to a user
     """
-    return f'My categories: {"; ".join(chain.from_iterable(ans))}'
+    return ('Ваши категории: '
+            f'{"; ".join(chain.from_iterable(ans))}'
+            )
 
 
 def create_message_select_query(ans):
@@ -38,24 +48,90 @@ def create_message_select_query(ans):
         pred_high_90_conf = i[9]
         actual_outcome = i[10]
 
-        even: str = "even" if n % 2 == 0 else "odd"
         text += (
-            f"<tr class={even}><td>{str(id)}</td><td>{date}</td><td>"
-            f"{task_description}</td><td>{task_category}</td><td>"
-            f"{unit_of_measure}</td><td>{str(pred_low_50_conf)}</td>"
-            f"<td>{str(pred_high_50_conf)}</td>"
-            f"<td>{str(pred_low_90_conf)}</td>"
-            f"<td>{str(pred_high_90_conf)}</td>"
-            f"<td>{str(actual_outcome)}</td></tr>"
+            f"<b>№: {str(id)}</b>\n"
+            f"<b>{date}</b>\n\n{PENCIL}"
+            f" {task_description}\n\n<b>Категория:</b> {task_category}\n"
+            f"<b>Единица измерения:</b> {unit_of_measure}\n\n"
+            "<b>ГРАНИЦЫ</b>\n"
+            f"{SMALL_DIAMOND} <b>Нижняя 50%:</b> {pred_low_50_conf}\n"
+            f"{SMALL_DIAMOND} <b>Верхняя 50%:</b> {pred_high_50_conf}\n"
+            f"{SMALL_DIAMOND} <b>Нижняя 90%:</b> {pred_low_90_conf}\n"
+            f"{SMALL_DIAMOND} <b>Верхняя 90%:</b> {pred_high_90_conf}\n\n"
+            f"{BLACK_SQUARE} <b>Результат:</b> {actual_outcome or ''}\n"
+            f"{'_' * SMILES_NUMBER}\n\n"
         )
+    message = "Предсказания, сделанные Вами на текущий момент:\n\n" + text
+    return message
+
+
+def one_message(ans: dict[str, str]):
+    """Create a message to show to a user.
+
+    Args:
+        ans (dict[str, str]): dictionary with inputed values
+    """
     message = (
-        "<html><head></head><body><table class='center'>"
-        "<tr><th>id</th><th>date</th><th>task description</th>"
-        "<th>task category</th><th>unit of measure</th>"
-        "<th>confidence 50 low</th><th>confidence 50 high</th>"
-        "<th>confidence 90 low</th><th>confidence 90 high</th>"
-        "<th>outcome</th></tr>"
-        f"{text}"
-        "</table></body></html>"
+        f"{PENCIL} {ans['prediction']}\n\n"
+        f"<b>Категория:</b> {ans['category']}\n"
+        f"<b>Единица измерения:</b> {ans['unit']}\n\n"
+        "<b>ГРАНИЦЫ</b>\n"
+        f"{SMALL_DIAMOND} <b>Нижняя 50%:</b> {ans['low_50']}\n"
+        f"{SMALL_DIAMOND} <b>Верхняя 50%:</b> {ans['hi_50']}\n"
+        f"{SMALL_DIAMOND} <b>Нижняя 90%:</b> {ans['low_90']}\n"
+        f"{SMALL_DIAMOND} <b>Верхняя 90%:</b> {ans['hi_90']}"
     )
     return message
+
+
+def check_click(load: str) -> bool:
+    """Check user's input isn't any of the buttons.
+
+    Args:
+        load (str): message we have got
+
+    Returns:
+        bool: True if its a regular message and False if
+        it's one of the bot buttons
+    """
+    commands = [
+        "Добавить предсказание",
+        "Обновить предсказание",
+        "Удалить предсказание",
+        "Результат предсказания",
+        "Проверить калибровку"
+    ]
+    for comm in commands:
+        if re.match(comm, load):
+            return False
+    return True
+
+
+async def err_message(
+    client,
+    who: Union[str, int],
+    parse_mode: str = "md",
+    state_dict: dict[str, str] = None,
+    mess: str = (
+        "Складывается впечатление, что вы еще не внесли ни одного"
+        " предсказания. Попробуйте."
+    ),
+    del_state: bool = True,
+):
+    """Send an error message to a user.
+
+    Args:
+        client (telethon.client): telethon.client object
+        who (Union[str, int]): a user's entity whom we should send the message
+        state_dict (dict[str, str]): dictionary with users' states.
+        Defaults to None
+        mess (str, optional): what message to send. Defaults to
+        ("Складывается впечатление,
+        что вы еще не внесли ни одного" " предсказания. Попробуйте." ).
+        del_state (bool, optional): whether we need to delete user's state.
+        Defaults to True.
+        parse_mode (str): message parse mode (html or markdown)
+    """
+    await client.send_message(who, mess, parse_mode=parse_mode)
+    if del_state:
+        del state_dict[who]
